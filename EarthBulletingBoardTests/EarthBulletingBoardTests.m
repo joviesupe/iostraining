f7ed5e7334abe957ac91e12d82f5018c2a097af6
Jovie Supe <joviesupe@seerlabs.com>
2014-12-09 09:52:13 +0800

    Initial Commit

---
+++ EarthBulletingBoardTests/EarthBulletingBoardTests.m
@@ -0,0 +1,40 @@
+//
+//  EarthBulletingBoardTests.m
+//  EarthBulletingBoardTests
+//
+//  Created by Jovie Supe on 12/9/14.
+//  Copyright (c) 2014 Jovie Francisco Supe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface EarthBulletingBoardTests : XCTestCase
+
+@end
+
+@implementation EarthBulletingBoardTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
